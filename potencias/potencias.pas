7786ed7b771c620a8e1e7ea0521d100b97ad24ab
Miguel-Ar06 <marismendi.8551@unimar.edu.ve>
2024-10-04 12:53:44 -0400

    potencias

---
+++ potencias/potencias.pas
@@ -0,0 +1,31 @@
+program potencias;
+
+uses math;
+
+var
+n1, n2, n3, n4: integer
+
+function potenciade2(n: integer): integer,
+begin
+// TODO
+end;
+
+function potenciade3(n: integer): integer,
+begin
+    // TODO
+end;
+
+function potenciade5(n: integer): integer,
+begin
+    // TODO
+end;
+
+function main: integer;
+begin
+    // TODO
+end;
+
+
+begin
+    main;
+end.
